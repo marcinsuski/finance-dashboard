e44e2d37518b632f72a2e521fd947e2119e32891
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2023-03-29 15:22:41 +0200

    node.js setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// MONGOOSE SETUP
+
